be7a70b777b59cb5fba9a6e023a9c01b3922e3ed
IashuraI <takenashura@gmail.com>
2022-02-25 12:16:53 +0200

    add odata models

--- backend/presentation/Md.WebApi/Program.cs
+++ backend/presentation/Md.WebApi/Program.cs
@@ -1,5 +1,12 @@
+using Md.Domain.Entities.Identity;
+using Md.Domain.Entities.Order;
+using Md.Domain.Entities.Product;
+using Md.Infrastucture.Meta.Services;
 using Md.Persistentce;
 using Microsoft.AspNetCore.OData;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
 using Microsoft.OpenApi.Models;
 using Serilog;
 
@@ -9,7 +16,10 @@ var cfg = builder.Configuration;
 var env = builder.Environment;
 
 builder.Services.AddControllers()
-    .AddOData(options => options.Select().Filter().OrderBy());
+    .AddOData(options => {
+        options.Select().Filter().OrderBy();
+        options.AddRouteComponents("odata", GetEdmModel());
+    });
 
 builder.Host.UseSerilog((ctx, cfg) =>
     cfg.WriteTo.Console());
@@ -18,6 +28,9 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Md.Bot.WebApi", Version = "v1" });
 });
+
+builder.Services.AddSingleton<IMemoryCache, MemoryCache>();
+builder.Services.AddSingleton<ResourceService>();
 
 builder.Services.AddPersistence(cfg);
 
@@ -40,3 +53,16 @@ app.UseEndpoints(endpoints =>
 });
 
 app.Run();
+
+IEdmModel GetEdmModel()
+{
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+
+    builder.EntitySet<Product>(nameof(Product));
+    builder.EntitySet<Order>(nameof(Order));
+    builder.EntitySet<Role>(nameof(Role));
+    builder.EntitySet<User>(nameof(User));
+    builder.EntitySet<Category>(nameof(Category));
+
+    return builder.GetEdmModel();
+}
